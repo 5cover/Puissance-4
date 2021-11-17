3f87d475947901fb6e236114aee7ebf2d7cdf00f
Raphaël Bardini <winclean.raphaelbardini@gmail.com>
2021-11-17 15:55:06 +0100

    afficherGrille ébauche

--- Puissance_4.py
+++ Puissance_4.py
@@ -1,1 +1,29 @@
+﻿from enum import Enum
 
+# Énumération de 3 valeurs
+class Case(Enum):
+    VIDE = ' '
+    JOUEUR_1 = '▮'
+    JOUEUR_2 = '▯'
+
+HAUTEUR = 6
+LARGEUR = 7
+
+grille = [[Case.VIDE] * (LARGEUR + 1)] * (HAUTEUR + 1)
+
+def AfficherGrille():
+
+    for ligne in range(HAUTEUR):
+
+        if ligne == 0:
+            print(f'   ┌{"─┬"*(LARGEUR-1)}─┐', end=f'\n {ligne} │')
+        else:
+            print(f'   ├{"─┼"*(LARGEUR-1)}─┤', end=f'\n {ligne} │')
+
+        for colonne in range(LARGEUR):
+            print(grille[colonne][ligne].value, end = '│')
+
+        print("\n", end="")
+
+    print(f'   └{"─┴"*(LARGEUR-1)}─┘')
+AfficherGrille()
